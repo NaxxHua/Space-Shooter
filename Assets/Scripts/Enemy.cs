ff7db25fe9989a8ffa5d72b033e0c59f11c82bfa
Jiahua Ma <naxxhua@gmail.com>
2022-10-19 17:15:58 -0500

    feat(enemy): enabled enemy to fire lasers and damage player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float _speed = 4.0f;
+    [SerializeField]
+    private float _speed = 1.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
 
     private Player _player;
 
     private Animator _anim;
 
     private AudioSource _audioSource;
+
+    private float _fireRate = 3.0f;
+    private float _canFire = -1;
 
 
     // Start is called before the first frame update
@@ -33,6 +39,24 @@ public class Enemy : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        CalculateMovement();
+
+        if(Time.time > _canFire)
+        {
+            _fireRate = Random.Range(3f, 7f);
+            _canFire = Time.time + _fireRate;
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
+
+            for (int i = 0; i < lasers.Length; i++)
+            {
+                lasers[i].AssignEnemyLaser();
+            }
+        }
+    }
+
+    void CalculateMovement()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 8f;
+    private float _speed = 8.0f;
+
+    private bool _isEnemyLaser = false;
 
     // Update is called once per frame
     void Update()
+    {
+        if (_isEnemyLaser == false) {
+            {
+                MoveUp();
+            }
+        }
+        else
+        {
+            MoveDown();
+        }
+    }
+
+    void MoveUp()
     {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
@@ -19,6 +34,37 @@ public class Laser : MonoBehaviour
                 Destroy(transform.parent.gameObject);
             }
             Destroy(this.gameObject);
+        }
+    }
+
+    void MoveDown()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -8f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void AssignEnemyLaser()
+    {
+        _isEnemyLaser = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player" && _isEnemyLaser == true)
+        {
+            Player player = other.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.Damage();
+            }
         }
     }
 }
