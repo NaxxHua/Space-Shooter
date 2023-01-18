7ad396a29a5b84adc5a6ae2cd9696218fda714d7
Jiahua Ma <naxxhua@gmail.com>
2023-01-18 17:35:40 -0600

    feat(coop): linked button to coop scene

--- Assets/Scripts/Main_Menu/MainMenu.cs
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void LoadGame()
+    public void LoadSinglePlayerGame()
     {
         SceneManager.LoadScene(1); // Game Scene
+    }
+
+    public void LoadCoopGame()
+    {
+        SceneManager.LoadScene(2); // Game Scene
     }
 }
